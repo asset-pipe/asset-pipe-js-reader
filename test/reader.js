ee248a9625527608d9e3bdb77e1f83d713cd7604
Trygve Lie <post@trygve-lie.com>
2016-11-21 15:28:02 +0100

    initial test setup

--- test/reader.js
+++ test/reader.js
@@ -1,34 +1,12 @@
 "use strict";
 
-const   stream      = require('readable-stream'),
-        request     = require('request'),
-        UglifyJS 	= require("uglify-js"),
-        reader 		= require('../');
-        
+const tap = require('tap');
+const Writer = require('../');
 
+tap.test('foo() - bar', function (t) {
+//    var writer = new Writer('./test/mock/main.js');
+//    writer.bundle().pipe(process.stdout);
 
-var src = [
-	request.get('http://127.0.0.1:7100/public/assets/main.json')
-
-	];
-
-var buffer = '';
-
-var dest = new stream.Writable({
-  write: function(chunk, encoding, next) {
-    // console.log(chunk.toString());
-    buffer += chunk.toString();
-    next()
-  }
+    t.equal(true, true);
+    t.end();
 });
-
-dest.on('finish', () => {
-  let start = process.hrtime();
-  let result = UglifyJS.minify(buffer, {fromString: true, compress: false});
-  let end = process.hrtime(start);
-  console.log('minfied', result.code);
-  console.log("minifying time:", end[0] + 'sec', end[1]/1000000 + 'ms');
-});
-
-
-reader(src).pipe(dest);
