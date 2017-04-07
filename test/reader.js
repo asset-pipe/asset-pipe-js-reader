8480a4fa56d04b1a9ecd0f6b1651ecd368d64eb4
Trygve Lie <post@trygve-lie.com>
2017-04-07 10:05:33 +0200

    Prevent linting from making travis fail

--- test/reader.js
+++ test/reader.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const tap = require('tap');
-const Reader = require('../'); // eslint-disable-line
-const fs = require('fs');
+// const Reader = require('../'); // eslint-disable-line
+// const fs = require('fs');
 
 tap.test('foo() - bar', (t) => {
-    const feedA = fs.createReadStream('./test/mock/feed.a.json');
-    const feedB = fs.createReadStream('./test/mock/feed.b.json');
+//    const feedA = fs.createReadStream('./test/mock/feed.a.json');
+//    const feedB = fs.createReadStream('./test/mock/feed.b.json');
 
-    const reader = new Reader([feedA, feedB]);
+//    const reader = new Reader([feedA, feedB]);
 //    reader.pipe(fs.createWriteStream('./browser.bundle.js'));
 
     t.equal(true, true);
